48e26971b0a21464e0432dcdc36f3cd10a1629c8
Jonathon Jongsma <jjongsma@redhat.com>
2025-07-25 09:46:05 -0500

    Improve logging
    Update to env_logger 0.11 and use format_source_path() so that the file
    and line number is included in the log messages for improved debugging.
    
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>

--- src/logger.rs
+++ src/logger.rs
@@ -4,5 +4,7 @@ pub fn logger() -> env_logger::Builder {
     let env = env_logger::Env::new()
         .filter_or("MDEVCTL_LOG", "warn")
         .write_style("MDEVCTL_LOG_STYLE");
-    env_logger::Builder::from_env(env)
+    let mut b = env_logger::Builder::from_env(env);
+    b.format_source_path(true);
+    b
 }
